97c82a5776fa960900cda14427e33d1532fad2b6
aish <nikam@123>
2023-11-14 17:10:13 +0530

    jwt added

--- backend/api/routes/userProfile.js
+++ backend/api/routes/userProfile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const User = require('../models/userProfile.js');
 
 router.get('/', (req, res) => {
@@ -9,7 +10,7 @@ router.get('/', (req, res) => {
   });
 });
 
-router.post('/', (req, res) => {
+router.post('/register', (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
@@ -42,45 +43,39 @@ router.post('/', (req, res) => {
   });
 });
 
-
-router.get('/', (req, res) => {
-  res.status(200).json({
-    msg: 'userProfile route is working'
-  });
-});
-
-
-router.post('/login', (req, res) => {
+router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
     return res.status(400).json({ error: "Email and password are required." });
   }
 
-  User.findOne({ email: email })
-    .exec()
-    .then(user => {
-      if (!user) {
-        return res.status(401).json({ error: "Authentication failed." });
-      }
+  try {
+    const user = await User.findOne({ email: email }).exec();
 
-      bcrypt.compare(password, user.password, (err, result) => {
-        if (err) {
-          return res.status(401).json({ error: "Authentication failed." });
-        }
+    if (!user) {
+      return res.status(401).json({ error: "Authentication failed." });
+    }
 
-        if (result) {
-          return res.status(200).json({ message: "Authentication successful." });
-        } else {
-          return res.status(401).json({ error: "Authentication failed." });
-        }
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      });
-    });
+    const result = await bcrypt.compare(password, user.password);
+
+    if (!result) {
+      return res.status(401).json({ error: "Wrong Password or username!" });
+    }
+
+    // Create a JWT token
+    const token = jwt.sign(
+      { userId: user._id, email: user.email },
+      'qovdk',
+      { expiresIn: '1h' } // You can adjust the expiration time
+    );
+
+    return res.status(200).json({ token: token, message: "Authentication successful." });
+
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ error: "Internal server error." });
+  }
 });
 
 
